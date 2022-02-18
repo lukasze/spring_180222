f09a0fb0c313f39cdf7e6716ced5fc8b697be9cb
uki <wyspianski.lukasz@gmail.com>
2022-02-18 15:00:06 +0100

    Test GET /hobbits -> HTTP 200

--- src/test/java/com/example/demo/controller/HobbitControllerTest.java
+++ src/test/java/com/example/demo/controller/HobbitControllerTest.java
@@ -1,26 +1,43 @@
 package com.example.demo.controller;
 
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-// supermoc - uruchom pełen kontekst Spring'a przed uruchomieniem testu
 @SpringBootTest
-// podpowiedz Spring'owi jak utworzyć ziarno MockMvc
 @AutoConfigureMockMvc
 class HobbitControllerTest {
-    // Wstrzukiwanie przez pole!! (w teście OK ;) )
+
     @Autowired
-    // API do testów endpointów
     private MockMvc mockMvc;
-    // adnotacja JUnit - metoda testowa
+
     @Test
+    @DisplayName("GET /hoobits -> HTTP 200")
+    @SneakyThrows
     void findAllHobbits() {
-        fail();
+
+        var endpoint = "/hobbits";
+
+        mockMvc
+                // wywołaj endpoint: metoda HTTP, nagłówek, media type etc.
+                .perform(
+                        // get zwraca obiekt, na którym możemy ustawiać nagłówek, ciało rządania etc.
+                        MockMvcRequestBuilders.get(endpoint)
+                )
+                // lepsze logowanie
+                .andDo(print())
+                // czego oczekujemy
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
 }
