8f41b03fc84c8088c251f75a58fe3774f1b7de70
uki <wyspianski.lukasz@gmail.com>
2022-02-18 11:05:13 +0100

    Test kontrollera {fail()]

---
+++ src/test/java/com/example/demo/controller/HobbitControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HobbitControllerTest {
+
+    @Test
+    void findAllHobbits() {
+        fail();
+    }
+
+}
