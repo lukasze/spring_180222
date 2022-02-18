a93c2817749e0acb464febd1dbe5ce09d8b8c15c
uki <wyspianski.lukasz@gmail.com>
2022-02-18 14:21:44 +0100

    Tuning generowania klucza, populacja bazy

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into hobbit (id, name, last_name) values (1, 'Frodo', 'Baggins');
+insert into hobbit (id, name, last_name) values (2, 'Bilbo', 'Baggins');
+insert into hobbit (id, name, last_name) values (3, 'Penelope', 'Tuk');
