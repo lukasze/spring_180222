5ddf7a428be94a872aefbf20bd30354456572166
uki <wyspianski.lukasz@gmail.com>
2022-02-18 14:42:16 +0100

    Przygotowanie klasy testowej: pełen kontekst Spring'a, MockMvc

--- src/test/java/com/example/demo/controller/HobbitControllerTest.java
+++ src/test/java/com/example/demo/controller/HobbitControllerTest.java
@@ -1,11 +1,23 @@
 package com.example.demo.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// supermoc - uruchom pełen kontekst Spring'a przed uruchomieniem testu
+@SpringBootTest
+// podpowiedz Spring'owi jak utworzyć ziarno MockMvc
+@AutoConfigureMockMvc
 class HobbitControllerTest {
-
+    // Wstrzukiwanie przez pole!! (w teście OK ;) )
+    @Autowired
+    // API do testów endpointów
+    private MockMvc mockMvc;
+    // adnotacja JUnit - metoda testowa
     @Test
     void findAllHobbits() {
         fail();
